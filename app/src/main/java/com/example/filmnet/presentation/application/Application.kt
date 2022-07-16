51839ab957b51e61304310ef9df2e871bb2d5d0c
Mahfouzian <alirezamahfouzian777@gmail.com>
2022-07-16 13:45:45 +0430

    add hilt to project

---
+++ app/src/main/java/com/example/filmnet/presentation/application/Application.kt
@@ -0,0 +1,7 @@
+package com.example.filmnet.presentation.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ApplicationClass : Application()
