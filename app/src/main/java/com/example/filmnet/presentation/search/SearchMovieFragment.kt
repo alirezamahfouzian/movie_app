b901e85f18ad30166b8c913d2211494dc3be7b4e
Mahfouzian <alirezamahfouzian777@gmail.com>
2022-07-16 13:25:49 +0430

    restructure packages

---
+++ app/src/main/java/com/example/filmnet/presentation/search/SearchMovieFragment.kt
@@ -0,0 +1,27 @@
+package com.example.filmnet.presentation.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.filmnet.databinding.FragmentSearchMovieBinding
+
+class SearchMovieFragment : Fragment() {
+
+    private var _binding: FragmentSearchMovieBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSearchMovieBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
